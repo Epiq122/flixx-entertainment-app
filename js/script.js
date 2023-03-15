f7bdef1e2a2687d32b20fc5a8300d9dd1638d37d
Robert Gleason <l2ob122@gmail.com>
2023-03-14 22:31:34 -0700

    created the fetchData function, created displayPopularmovies function and got it to work to display popular movies om the page

--- js/script.js
+++ js/script.js
@@ -4,6 +4,55 @@ const global = {
 };
 console.log(global.currentPage);
 
+// display the popular movies
+const displayPopularMovies = async () => {
+  const { results } = await fetchAPIData('movie/popular');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+				${
+          movie.poster_path
+            ? `	<img
+							src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+							class="card-img-top"
+							alt="${movie.title}"
+                />`
+            : `	<img
+							src="../images/no-image.jpg"
+							class="card-img-top"
+							alt="${movie.title}"
+					/>`
+        }
+				</a>
+				<div class="card-body">
+					<h5 class="card-title">${movie.title}</h5>
+					<p class="card-text">
+						<small class="text-muted">Release: ${movie.release_date}</small>
+					</p>
+				</div>
+
+
+
+    `;
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+};
+
+// fetch data from the TMDB API
+const fetchAPIData = async (endpoint) => {
+  const API_KEY = '3b5978dd09b03850afb94675ec87b1eb';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`,
+  );
+  const data = await response.json();
+  return data;
+};
+
 // Highlight active link
 const highlightActiveLink = () => {
   const links = document.querySelectorAll('.nav-link');
@@ -19,7 +68,7 @@ const init = () => {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
