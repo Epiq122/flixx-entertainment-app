2d73e0a20657f0195b897780e1de6aee73c015dc
Robert Gleason <l2ob122@gmail.com>
2023-03-15 21:10:44 -0700

    created backdrop function for the movies and tv shows

--- js/script.js
+++ js/script.js
@@ -85,6 +85,9 @@ const displayMovieDetails = async () => {
 
   const movie = await fetchAPIData(`movie/${movieId}`);
 
+  // overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
 
   div.innerHTML = ` <div class="details-top">
@@ -142,6 +145,28 @@ const displayMovieDetails = async () => {
             .join(' ')}</div>
         </div>`;
   document.querySelector('#movie-details').appendChild(div);
+};
+
+// display backdrop on movie and tv show details page
+
+const displayBackgroundImage = (type, backgroundPath) => {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+  if (type == 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 };
 
 // fetch data from the TMDB API
