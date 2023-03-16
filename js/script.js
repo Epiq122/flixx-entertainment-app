942c0a35dd180d4afa37497c3df8040a08bfe755
Robert Gleason <l2ob122@gmail.com>
2023-03-15 19:54:21 -0700

    created spinner functiona and displayTvShows

--- js/script.js
+++ js/script.js
@@ -41,16 +41,63 @@ const displayPopularMovies = async () => {
   });
 };
 
+// display the popular tvshows
+const displayPopularTvShows = async () => {
+  const { results } = await fetchAPIData('tv/popular');
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <a href="tv-details.html?id=${show.id}">
+				${
+          show.poster_path
+            ? `	<img
+							src="https://image.tmdb.org/t/p/w500/${show.poster_path}"
+							class="card-img-top"
+							alt="${show.name}"
+                />`
+            : `	<img
+							src="../images/no-image.jpg"
+							class="card-img-top"
+							alt="${show.name}"
+					/>`
+        }
+				</a>
+				<div class="card-body">
+					<h5 class="card-title">${show.name}</h5>
+					<p class="card-text">
+						<small class="text-muted">Air Date: ${show.first_air_date}</small>
+					</p>
+				</div>
+
+
+
+    `;
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+};
+
 // fetch data from the TMDB API
 const fetchAPIData = async (endpoint) => {
   const API_KEY = '3b5978dd09b03850afb94675ec87b1eb';
   const API_URL = 'https://api.themoviedb.org/3/';
 
+  showSpinner();
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`,
   );
   const data = await response.json();
+  hideSpinner();
   return data;
+};
+
+const showSpinner = () => {
+  document.querySelector('.spinner').classList.add('show');
+};
+
+const hideSpinner = () => {
+  document.querySelector('.spinner').classList.remove('show');
 };
 
 // Highlight active link
@@ -71,7 +118,7 @@ const init = () => {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularTvShows();
     case '/movie-details.html':
       console.log('Movie-Details');
     case '/tv-details.html':
